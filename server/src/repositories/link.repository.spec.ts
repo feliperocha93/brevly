abb15fa65429f9fd8df64b887b9b002090d8dde4
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-04-12 00:01:10 -0300

    feat: update column to short_url and create repository to insert link

--- server/src/repositories/link.repository.spec.ts
+++ server/src/repositories/link.repository.spec.ts
@@ -1,23 +1,29 @@
-import { describe, it, expect } from 'vitest'
-import { findByShortenedUrl, insertLink } from './link.repository.ts'
+import { describe, it, expect, afterEach } from 'vitest'
+import { insertLink } from './link.repository.ts'
 import { db } from '@/db/index.ts'
 import { schema } from '@/db/schemas/index.ts'
 import { ilike } from 'drizzle-orm'
 
 describe('Link Repository', () => {
+    afterEach(async () => {
+        await db.delete(schema.links);
+    })
+
     describe('insertLink', () => {
         it('should insert a new link into the database', async () => {
-            // Arrange
             const originalUrl = 'https://example.com'
-            const shortenedUrl = 'abc1234'
+            const shortUrl = 'https://brevly/123'
 
-            // Act
-            await insertLink(originalUrl, shortenedUrl)
+            const insertedLink = await insertLink(originalUrl, shortUrl)
 
-            const result = await db.select().from(schema.links).where(ilike(schema.links.shortenedUrl, shortenedUrl)).limit(1)
+            expect(insertedLink.id).toBeDefined()
+            expect(insertedLink.originalUrl).toBe(originalUrl)
+            expect(insertedLink.shortUrl).toBe(shortUrl)
+            expect(insertedLink.accessCount).toBe(0)
+            expect(insertedLink.createdAt).toBeDefined()
 
+            const result = await db.select().from(schema.links).where(ilike(schema.links.shortUrl, shortUrl)).limit(1)
             expect(result[0].originalUrl).toBe(originalUrl)
-            expect(result[0].shortenedUrl).toBe(shortenedUrl)
         })
     })
 
