9e8ea98d0a58825d8a7360f0b97a7c9228ce91ed
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-04-16 12:26:11 -0300

    chore: update bucket name to test integration

--- server/iac/pulumi/index.ts
+++ server/iac/pulumi/index.ts
@@ -1,7 +1,7 @@
 import * as cloudflare from ".pnpm/@pulumi+cloudflare@6.0.0_typescript@5.8.3/node_modules/@pulumi/cloudflare";
 
 const exportLinksBucket = new cloudflare.R2Bucket("exportLinksBucket", {
-    name: "export-links",
+    name: "export-links-bucker",
     accountId: "f4a0805829dc6a08e153fa9419f5a400",
 });
 
