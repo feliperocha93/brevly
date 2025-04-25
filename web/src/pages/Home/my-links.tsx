d97e7e940a5712da9c07f5d9c71a6ad233d60dc1
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-04-25 20:14:06 -0300

    feat: export links to csv

--- web/src/pages/Home/my-links.tsx
+++ web/src/pages/Home/my-links.tsx
@@ -2,6 +2,7 @@ import { MyLinksEmptyState } from "./my-links-empty-state";
 import { MyLinksItem } from "./my-links-item";
 
 import { Button } from "../../components/ui/button";
+import { useExportLinks } from "../../hooks/useExportLinks";
 
 import IconDonwload from "../../assets/icons/DownloadSimple.svg";
 
@@ -12,11 +13,16 @@ interface MyLinksProps {
 export function MyLinks(
     {links}
 : MyLinksProps) {
+    const { exportLinks } = useExportLinks();
+
     return (
         <div className="bg-white w-full rounded-lg flex flex-col gap-4 md:gap-6 p-6 md:p-8">
             <div className="flex items-center justify-between">
                 <h2 className="text-lg font-bold text-gray-600">Meus Links</h2>
-                <Button variant="secondary">
+                <Button
+                    variant="secondary"
+                    onClick={exportLinks}
+                >
                     <img src={IconDonwload} alt="Download CSV" className="w-4 h-4" />
                     <span>Baixar CSV</span>
                 </Button>
