df0bdc60085b6c7d4f346bd39812edf7a2a47541
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-05-02 09:30:06 -0300

    chore: setup radix theme

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,10 @@
+import App from '@/App.tsx'
+import '@/index.css'
+import "@radix-ui/themes/styles.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from '@/App.tsx'
-import '@/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
