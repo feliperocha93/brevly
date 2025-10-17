ddd2967eacab1ccf48b5f958ae70dc99503ad368
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-10-17 08:11:33 -0300

    chore: remove iac

--- server/infra/iac/pulumi/index.ts
+++
@@ -1,8 +0,0 @@
-import * as cloudflare from "@pulumi/cloudflare";
-
-const exportLinksBucket = new cloudflare.R2Bucket("exportLinksBucket", {
-    name: "export-links-bucket",
-    accountId: "f4a0805829dc6a08e153fa9419f5a400",
-});
-
-export const exportLinksBucketName = exportLinksBucket.name;
